01e1384521b487ec255fdaa78dc6aa1c9124d281
Farid Saleh <github.farid1@gmail.com>
2023-07-28 10:45:43 +0900

    update scripts

--- alihmedia_inactive/views.py
+++ alihmedia_inactive/views.py
@@ -8,7 +8,8 @@ from os.path import exists
 from django.conf import settings
 import inspect
 import sys
-              
+import time
+from datetime import datetime
 def getmenu():
     return Department.objects.all()
 
@@ -254,7 +255,7 @@ def pdfdownload(request, link, doc_id):
             return response
     raise Http404    
 
-def statistics(request):
+def statistics_old(request):
     deps = Department.objects.all()
     depnamelist = []
     depvaluelist = []
@@ -278,12 +279,11 @@ def statistics(request):
         depnamelist.append(" | ".join([d.name, "Belum"]))
         depvaluelist.append(found)
         depvaluelist.append(notfound)
-        colorlist.append("rgba(41,134,204,1.000)")
-        colorlist.append("rgba(244,204,204,1.000)")
+        colorlist.append("rgba(112, 185, 239, 1)")
+        colorlist.append("rgba(244, 204, 204, 1)")
         total = foundall + notfoundall
         procfound = foundall / total * 100
         procnotfound = notfoundall / total * 100
-
     context = {
         "menu": getmenu(),
         "depnamelist": depnamelist,
@@ -296,3 +296,100 @@ def statistics(request):
     }
 
     return render(request=request, template_name='alihmedia_inactive/statistics.html', context=context)
+
+
+
+def statistics(request):
+    deps = Department.objects.all()
+    depnamelist = []
+    depvaluelist = []
+    colorlist = []
+    foundall = 0
+    notfoundall = 0
+    for d in deps:
+        foundlist = [os.path.join(root, file) for root, dirs, files in os.walk(os.path.join(settings.PDF_LOCATION, __package__.split('.')[0], d.link)) for file in files if file.endswith(".pdf")]
+        found = len(foundlist)
+        foundall += found
+        docs = Doc.objects.filter(bundle__department_id__exact=d.id)
+        notfound = len(docs) - found
+        notfoundall += notfound
+        depnamelist.append(" | ".join([d.name, "Sudah"]))
+        depnamelist.append(" | ".join([d.name, "Belum"]))
+        depvaluelist.append(found)
+        depvaluelist.append(notfound)
+        colorlist.append("rgba(112, 185, 239, 1)")
+        colorlist.append("rgba(244, 204, 204, 1)")
+        total = foundall + notfoundall
+        procfound = foundall / total * 100
+        procnotfound = notfoundall / total * 100
+    
+    fileinfolist = []
+    allfilelist = [os.path.join(root, file) for root, dirs, files in os.walk(os.path.join(settings.PDF_LOCATION, __package__.split('.')[0])) for file in files if file.endswith(".pdf")]
+    for filepath in allfilelist:
+        infotime = os.path.getmtime(filepath)
+        infodate = datetime.fromtimestamp(infotime).strftime('%d-%m-%Y')
+        mdict = {
+            "file": filepath,
+            "date": infodate
+        }
+        fileinfolist.append(mdict)
+    groupdates = {}
+    docdate = []
+    docscan = []
+    doccolor = []
+    for item in fileinfolist:
+        groupdates.setdefault(item['date'], []).append(item)
+
+    for key, value in groupdates.items():
+        docdate.append(key)
+        docscan.append(len(value))
+        doccolor.append("rgba(112, 185, 239, 1)")
+
+    context = {
+        "menu": getmenu(),
+        "depnamelist": depnamelist,
+        "depvaluelist": depvaluelist,
+        "colorlist": colorlist,
+        "foundall": str(foundall),
+        "notfoundall": str(notfoundall),
+        "procfound":f"{procfound:.3f}",
+        "procnotfound":f"{procnotfound:.3f}",
+        "docdate": docdate,
+        "docscan": docscan,
+        "doccolor": doccolor,
+    }
+
+    return render(request=request, template_name='alihmedia_inactive/statistics.html', context=context)
+
+
+
+def tes1(request):
+    fileinfolist = []
+    allfilelist = [os.path.join(root, file) for root, dirs, files in os.walk(os.path.join(settings.PDF_LOCATION, __package__.split('.')[0])) for file in files if file.endswith(".pdf")]
+    for filepath in allfilelist:
+        infotime = os.path.getmtime(filepath)
+        infodate = datetime.fromtimestamp(infotime).strftime('%d-%m-%Y')
+        mdict = {
+            "file": filepath,
+            "date": infodate
+        }
+        fileinfolist.append(mdict)
+
+    d = {}
+    
+    for item in fileinfolist:
+        d.setdefault(item['date'], []).append(item)
+    
+
+    return HttpResponse( d.values())
+
+def tes2(request):
+    l = []
+    foundlist = [os.path.join(root, file) for root, dirs, files in os.walk(os.path.join(settings.PDF_LOCATION, __package__.split('.')[0], "irigasi")) for file in files if file.endswith(".pdf")]
+    for filepath in foundlist:
+        infotime = os.path.getmtime(filepath)
+        infodate = datetime.fromtimestamp(infotime).strftime('%d-%m-%Y')
+        # infodate = time.ctime(infotime)
+        l.append(filepath + " | " + infodate)
+
+    return HttpResponse("<br/>".join(l))
--- tes.py
+++ tes.py
@@ -1,2 +1,18 @@
-from os import path
-print(__file__)
+thedict = [
+    {"name": "item1", "price": 10},
+    {"name": "item2", "price": 5},
+    {"name": "item3", "price": 10},
+    {"name": "item4", "price": 12},
+    {"name": "item5", "price": 12},
+    {"name": "item6", "price": 5}
+ ]
+ 
+ 
+d = {}
+ 
+for item in thedict:
+    d.setdefault(item['price'], []).append(item)
+ 
+# list(d.values())
+ 
+print(d)
